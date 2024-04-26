47d587df9004e23839dd9a70dba9e23bc6a2abc6
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2024-04-25 20:08:48 -0400

    mi tienda

---
+++ storeApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from storeApp.models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
